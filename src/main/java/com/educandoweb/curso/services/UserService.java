7369e109c2644531f794d055200ec62ddd849680
CarlosEhduardo <carlos0eduardo074@gmail.com>
2022-04-09 14:58:19 -0300

    Exception handling - update

--- src/main/java/com/educandoweb/curso/services/UserService.java
+++ src/main/java/com/educandoweb/curso/services/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -42,10 +43,14 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entityMonitored = userRepository.getOne(id);
-        updateData(entityMonitored, obj);
+        try {
+            User entityMonitored = userRepository.getOne(id);
+            updateData(entityMonitored, obj);
 
-        return userRepository.save(entityMonitored);
+            return userRepository.save(entityMonitored);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entityMonitored, User obj) {
